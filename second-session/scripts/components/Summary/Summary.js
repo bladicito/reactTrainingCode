8db1e81c12223ae0ac83c6159396b5df0000387a
Bladimir Ardiles <bladi_sys@hotmail.com>
2016-04-28 16:35:52 +0200

    small changes in texts
    styling changed.

--- second-session/scripts/components/Summary/Summary.js
+++ second-session/scripts/components/Summary/Summary.js
@@ -29,29 +29,29 @@ var Summary = React.createClass({
                         <div className="col-md-4 col-xs-12">
                             <div className="summary__numbers">
                                 <div className="summary__numbers__item summary__numbers__item--matches-played">
-                                    <span className="summary__numbers__item--label">MATCHES PLAYED :</span> {this.props.playedGames}
+                                    <span className="summary__numbers__item--label">MATCHES PLAYED :</span>&nbsp;<span className="summary__numbers__item--text">{this.props.playedGames}</span>
                                 </div>
                                 <div className="summary__numbers__item summary__numbers__item--goals">
-                                    <span className="summary__numbers__item--label">GOALS</span> SCORED: {this.props.goals}
+                                    <span className="summary__numbers__item--label">GOALS SCORED:</span>&nbsp;<span className="summary__numbers__item--text">{this.props.goals}</span>
                                 </div>
                                 <div className="summary__numbers__item  summary__numbers__item--assists">
-                                    <span className="summary__numbers__item--label">ASSISTS IN SEASON:</span> {this.props.assists}
+                                    <span className="summary__numbers__item--label">ASSISTS IN SEASON:</span>&nbsp;<span className="summary__numbers__item--text"> {this.props.assists}</span>
                                 </div>
                                 <div className="summary__numbers__item  summary__numbers__item--yellow-cards">
-                                    <span className="summary__numbers__item--label">YELLOW CARDS IN SEASON:</span> {this.props.yellowCards}
+                                    <span className="summary__numbers__item--label">YELLOW CARDS IN SEASON:</span> &nbsp;<span className="summary__numbers__item--text">{this.props.yellowCards}</span>
                                 </div>
                                 <div className="summary__numbers__item summary__numbers__item--red-cards">
-                                    <span className="summary__numbers__item--label">RED CARDS IN SEASON:</span> {this.props.redCards}
+                                    <span className="summary__numbers__item--label">RED CARDS IN SEASON:</span>&nbsp;<span className="summary__numbers__item--text"> {this.props.redCards}</span>
                                 </div>
                                 <div className="summary__numbers__item  summary__numbers__item--played-minutes">
-                                    <span className="summary__numbers__item--label">MINUTES PLAYED:</span> {this.props.playedMinutes}
+                                    <span className="summary__numbers__item--label">MINUTES PLAYED:</span>&nbsp;<span className="summary__numbers__item--text">{this.props.playedMinutes}</span>
                                 </div>
 
                                 <div className="summary__numbers__item  summary__numbers__item--goals-each-minute">
-                                    <span className="summary__numbers__item--label">GOAL SCORED EACH </span> {Math.round(this.props.averageGoalsPerMinutes * 100) / 100} MINUTES
+                                    <span className="summary__numbers__item--label">GOAL SCORED EACH &nbsp;<span className="summary__numbers__item--text"></span> {Math.round(this.props.averageGoalsPerMinutes * 100) / 100} MINUTES</span>
                                 </div>
                                 <div className="summary__numbers__item  summary__numbers__item--effectiveness">
-                                    <span className="summary__numbers__item--label">EFFECTIVENESS:</span> {Math.round(this.props.averageGoalsPerMatch * 100) / 100} GOALS/GAME
+                                    <span className="summary__numbers__item--label">EFFECTIVENESS:</span>&nbsp;<span className="summary__numbers__item--text">{Math.round(this.props.averageGoalsPerMatch * 100) / 100} GOALS/GAME</span>
                                 </div>
                             </div>
 
