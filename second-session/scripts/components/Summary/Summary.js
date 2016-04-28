04b2faec6a24ea4f4acdcf39e26843196944ef76
Bladimir Ardiles <bladi_sys@hotmail.com>
2016-04-28 23:11:20 +0200

    set new grid for summary.

--- second-session/scripts/components/Summary/Summary.js
+++ second-session/scripts/components/Summary/Summary.js
@@ -26,7 +26,7 @@ var Summary = React.createClass({
                         <div className="col-md-4 col-xs-12">
                             {this.getPicture(this.props.mainImage, 'summary__image', '/build/img/')}
                         </div>
-                        <div className="col-md-4 col-xs-12">
+                        <div className="col-md-8 col-xs-12">
                             <div className="summary__numbers">
                                 <div className="summary__numbers__item summary__numbers__item--matches-played">
                                     <span className="summary__numbers__item--label">MATCHES PLAYED :</span>&nbsp;<span className="summary__numbers__item--text">{this.props.playedGames}</span>
@@ -48,16 +48,31 @@ var Summary = React.createClass({
                                 </div>
 
                                 <div className="summary__numbers__item  summary__numbers__item--goals-each-minute">
-                                    <span className="summary__numbers__item--label">GOAL SCORED EACH &nbsp;<span className="summary__numbers__item--text"></span> {Math.round(this.props.averageGoalsPerMinutes * 100) / 100} MINUTES</span>
+                                    <span className="summary__numbers__item--label">
+                                        GOAL SCORED EACH
+                                    </span>
+                                    <span className="summary__numbers__item--text">
+                                        {Math.round(this.props.averageGoalsPerMinutes * 100) / 100}
+                                    </span>
+                                    <span className="summary__numbers__item--label">
+                                        MINUTES
+                                    </span>
                                 </div>
                                 <div className="summary__numbers__item  summary__numbers__item--effectiveness">
-                                    <span className="summary__numbers__item--label">EFFECTIVENESS:</span>&nbsp;<span className="summary__numbers__item--text">{Math.round(this.props.averageGoalsPerMatch * 100) / 100} GOALS/GAME</span>
+                                    <span className="summary__numbers__item--label">
+                                        EFFECTIVENESS:
+                                    </span>
+                                    <span className="summary__numbers__item--text">
+                                        {Math.round(this.props.averageGoalsPerMatch * 100) / 100}
+                                    </span>
+                                    <span className="summary__numbers__item--label">
+                                        GOALS/GAME
+                                    </span>
+                                </div>
+                                <div className="summary__numbers__item  summary__numbers__item--club-logo">
+                                    {this.getPicture(Helpers.nameToImageShield(this.props.currentClub), 'summary-shield', '/build/svg/')}
                                 </div>
                             </div>
-
-                        </div>
-                        <div className="col-md-4 col-xs-12">
-                            {this.getPicture(Helpers.nameToImageShield(this.props.currentClub), 'summary-shield', '/build/svg/')}
                         </div>
                     </div>
                 </div>
